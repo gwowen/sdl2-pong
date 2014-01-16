0e149e734f244148f3781ca2250b9ae21dcc660c
Gareth <gareth.w.owen@gmail.com>
2014-01-16 20:24:32 +0000

    created a file called imagehandler instead... better name

---
+++ imagehandler.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   imagehandler.h
+ * Author: gareth
+ *
+ * Created on 16 January 2014, 20:24
+ */
+
+#ifndef IMAGEHANDLER_H
+#define	IMAGEHANDLER_H
+
+
+
+#endif	/* IMAGEHANDLER_H */
+
