7f193536415f85338737ccaea55dd9e7e631476f
Gareth <gareth.w.owen@gmail.com>
2014-01-18 18:20:43 +0000

    the name imagehandler sucked, replacing it with graphics instead

---
+++ graphics.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   graphics.h
+ * Author: gareth
+ *
+ * Created on 18 January 2014, 18:09
+ */
+
+#ifndef GRAPHICS_H
+#define	GRAPHICS_H
+
+class Graphics {
+    
+};
+
+
+
+#endif	/* GRAPHICS_H */
+
