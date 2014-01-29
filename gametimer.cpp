d137606bc09c4fff7a36458a0795cc1c1b1a43ee
Gareth <gareth.w.owen@gmail.com>
2014-01-29 23:39:30 +0000

    added gametimer class

---
+++ gametimer.cpp
@@ -0,0 +1,33 @@
+#include "gametimer.h"
+
+gameTimer::gameTimer() {
+    
+}
+
+void gameTimer::startGameTimer() {
+    
+}
+
+void gameTimer::stopGameTimer() {
+    
+}
+
+void gameTimer::getTime() {
+    
+}
+
+void gameTimer::pauseGameTimer() {
+    
+}
+
+void gameTimer::unpauseGameTimer() {
+    
+}
+
+bool gameTimer::checkStarted() {
+    
+}
+
+bool gameTimer::checkPaused() {
+    
+}
