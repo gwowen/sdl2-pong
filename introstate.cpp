641a669aef480540fc9772c66ccc70ce43e185a3
Gareth <gareth.w.owen@gmail.com>
2014-02-02 02:20:59 +0000

    added introstate and title screen

---
+++ introstate.cpp
@@ -0,0 +1,22 @@
+#include <SDL/SDL.h>
+#include "gameengine.h"
+#include "playstate.h"
+#include "introstate.h"
+
+introState introState::m_introState;
+
+void introState::Init() {
+    
+}
+
+void introState::Cleanup() {
+    
+}
+
+void introState::handleEvents(gameEngine* game) {
+    
+}
+
+void introState::Draw(gameEngine* game) {
+    
+}
---
+++ introstate.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   introstate.h
+ * Author: gareth
+ *
+ * Created on 02 February 2014, 00:10
+ */
+
+#ifndef INTROSTATE_H
+#define	INTROSTATE_H
+
+#include <SDL2/SDL.h>
+#include "gamestate.h"
+
+class introState : public gameState {
+    public:
+        void Init();
+        void Cleanup();
+        void handleEvents( gameEngine* game );
+        void Draw( gameEngine* game);
+        
+        static introState* Instance() {
+            return &m_introState;
+        }
+        
+    protected:
+        introState();
+        
+    private:
+        static introState m_introState;
+        SDL_Texture* introScreen;
+};
+
+
+
+#endif	/* INTROSTATE_H */
+
